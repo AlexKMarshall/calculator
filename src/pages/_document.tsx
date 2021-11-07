653fc9238dbf5bc0815c8e478dd86d13ab41c170
Alex Marshall <alex.k.marshall83@gmail.com>
2021-11-07 11:41:54 +0000

    remove boilerplate

---
+++ src/pages/_document.tsx
@@ -0,0 +1,34 @@
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
+
+class CustomDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default CustomDocument;
---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Calculator</title>
+        <meta
+          name="description"
+          content="Simple calculator app made with Next.js"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>Calculator</main>
+    </>
+  );
+};
+
+export default Home;
